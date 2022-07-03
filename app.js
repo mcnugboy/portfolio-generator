fe046d431dc4663e97ca8fa761f44af899d2853d
space cadet grant <53055825+mcnugboy@users.noreply.github.com>
2022-07-02 20:27:51 -0500

    Added inquirer to app.js.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template');
+import inquirer from 'inquirer'
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw;
+
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
